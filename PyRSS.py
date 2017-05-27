00c97fbc4b930664a6257f2d1475c59b074c9b21
F.K.Z <farooghkarimizadeh@gmail.com>
2017-05-27 20:18:57 +0430

    Added license information

--- PyRSS.py
+++ PyRSS.py
@@ -1,7 +1,20 @@
-# PyRSS Reader v0.0.1
-# https://github.com/rayeshman/PyRSS-Reader
-# See Licence
+'''
+    PyRSS Reader - a simple RSS reader written in Python with feedparser and GTK+
+    Copyright (C) 2016, 2017  Rayeshman <rayeshman's email here>
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
 from gi.repository import Gtk,WebKit
 import feedparser,socket
 
